e939a5a45ab4645a092cafc494750efccabe7d8f
jpicau <jpi@informatik.uni-kiel.de>
2012-11-15 16:28:50 +0100

    preparing geometry files

--- prepareMetos3DData.m
+++ prepareMetos3DData.m
@@ -24,70 +24,27 @@ function prepareMetos3DData
 format compact
 
 % prepare data files
-disp('Prepare data files ...');
-uncompressDataFile('Matrix');
-uncompressDataFile('Misc');
-uncompressDataFile('MITgcm');
-uncompressDataFile('PETSC');
-uncompressDataFile('MIT_Matrix_Global_2.8deg');
+prepareDataFiles();
 
 % set base path
-disp('Set base path ...');
-cd 'MIT_Matrix_Global_2.8deg/'
-base_path = pwd
-save basepath.mat base_path
-cd 'KielBiogeochem_NDOP_Matrix5_4/'
-save basepath.mat base_path
-cd ..
-cd ..
+setBasePath();
 
-% copy
-disp('Copy files ...');
-copyfile('Matrix/gcm2matrix.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/gcm2matrix.m')
-copyfile('Misc/read_binary.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/read_binary.m')
-copyfile('Misc/write_binary.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/write_binary.m')
-copyfile('MITgcm/rdmds.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/rdmds.m')
-copyfile('PETSC/writePetscBin.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/writePetscBin.m')
-copyfile('util/prep/writePetscBinaryMat_mex.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/writePetscBinaryMat_mex.m')
-copyfile('util/prep/convertBoundaryConditionsToPETSc.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/convertBoundaryConditionsToPETSc.m')
-copyfile('util/prep/convertIndices.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/convertIndices.m')
-copyfile('util/prep/convertHeightsToDepths.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/convertHeightsToDepths.m')
+% copy needed files
+copyNeededFiles();
 
 % run script from Samar Khatiwala
-disp('Run SPK script ...');
+disp('Run script from Samar Khatiwala ...');
 cd 'MIT_Matrix_Global_2.8deg/'
 cd 'KielBiogeochem_NDOP_Matrix5_4/'
-prep_files_for_petsc_kiel_biogeochem_timestepping_cg
+prep_files_for_petsc_kiel_biogeochem_timestepping_cg;
 cd ..
 cd ..
 
-% convert
-disp('Convert data ...');
-cd 'MIT_Matrix_Global_2.8deg/'
-cd 'KielBiogeochem_NDOP_Matrix5_4/'
-% latitude
-convertBoundaryConditionsToPETSc(1, 'latitude.bin', 4448);
-% fice
-convertBoundaryConditionsToPETSc(12, 'fice_', 4448);
-% indices
-convertIndices
-% depths
-convertHeightsToDepths
-cd ..
-cd ..
+% convert data to PETSc format
+convertDataToPETScFormat();
 
 % make dir structure
-disp('Make directory structure ...');
-mkdir('Metos3DData/2.8')
-mkdir('Metos3DData/2.8/Forcing')
-mkdir('Metos3DData/2.8/Forcing/BoundaryCondition')
-mkdir('Metos3DData/2.8/Forcing/BoundaryCondition/Special')
-mkdir('Metos3DData/2.8/Forcing/DomainCondition')
-mkdir('Metos3DData/2.8/Geometry')
-mkdir('Metos3DData/2.8/Initialization')
-mkdir('Metos3DData/2.8/Transport')
-mkdir('Metos3DData/2.8/Transport/Matrix5_4')
-mkdir('Metos3DData/2.8/Transport/Matrix5_4/1dt')
+makeDirectoryStructure();
 
 % copy into dir structure
 disp('Copy into directory structure ...');
@@ -96,14 +53,16 @@ disp('Copy into directory structure ...');
 disp('Transport ...');
 % explicit matrix
 for imatrix = 0 : 11
-    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/Ae_' sprintf('%02d', imatrix)]
-    fileNameTo   = ['Metos3DData/2.8/Transport/Matrix5_4/1dt/Ae_' sprintf('%02d.petsc', imatrix)]
+    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/Ae_' sprintf('%02d', imatrix)];
+    fileNameTo   = ['Metos3DData/2.8/Transport/Matrix5_4/1dt/Ae_' sprintf('%02d.petsc', imatrix)];
+    disp(fileNameTo);
     copyfile(fileNameFrom, fileNameTo);
 end
 % implicit matrix
 for imatrix = 0 : 11
-    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/Ai_' sprintf('%02d', imatrix)]
-    fileNameTo   = ['Metos3DData/2.8/Transport/Matrix5_4/1dt/Ai_' sprintf('%02d.petsc', imatrix)]
+    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/Ai_' sprintf('%02d', imatrix)];
+    fileNameTo   = ['Metos3DData/2.8/Transport/Matrix5_4/1dt/Ai_' sprintf('%02d.petsc', imatrix)];
+    disp(fileNameTo);
     copyfile(fileNameFrom, fileNameTo);
 end
 
@@ -113,8 +72,9 @@ disp('Boundary conditions ...');
 copyfile('util/prep/fukushima.petsc', 'Metos3DData/2.8/Forcing/BoundaryCondition/Special/fukushima.petsc');
 % ice cover
 for ivector = 0 : 11
-    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/fice_' sprintf('%02d.petsc', ivector)]
-    fileNameTo   = ['Metos3DData/2.8/Forcing/BoundaryCondition/fice_' sprintf('%02d.petsc', ivector)]
+    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/fice_' sprintf('%02d.petsc', ivector)];
+    fileNameTo   = ['Metos3DData/2.8/Forcing/BoundaryCondition/fice_' sprintf('%02d.petsc', ivector)];
+    disp(fileNameTo);
     copyfile(fileNameFrom, fileNameTo);
 end
 % latitude
@@ -128,22 +88,25 @@ copyfile('MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/dz.petsc', 'Met
 copyfile('MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/z.petsc', 'Metos3DData/2.8/Forcing/DomainCondition/z.petsc');
 % salinity
 for ivector = 0 : 11
-    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/Ss_' sprintf('%02d', ivector)]
-    fileNameTo   = ['Metos3DData/2.8/Forcing/DomainCondition/Ss_' sprintf('%02d.petsc', ivector)]
+    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/Ss_' sprintf('%02d', ivector)];
+    fileNameTo   = ['Metos3DData/2.8/Forcing/DomainCondition/Ss_' sprintf('%02d.petsc', ivector)];
+    disp(fileNameTo);
     copyfile(fileNameFrom, fileNameTo);
 end
 % temperature
 for ivector = 0 : 11
-    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/Ts_' sprintf('%02d', ivector)]
-    fileNameTo   = ['Metos3DData/2.8/Forcing/DomainCondition/Ts_' sprintf('%02d.petsc', ivector)]
+    fileNameFrom = ['MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/Ts_' sprintf('%02d', ivector)];
+    fileNameTo   = ['Metos3DData/2.8/Forcing/DomainCondition/Ts_' sprintf('%02d.petsc', ivector)];
+    disp(fileNameTo);
     copyfile(fileNameFrom, fileNameTo);
 end
 
 % geometry
 disp('Geometry ...');
 % indices
-copyfile('MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/gStartIndicesNew.bin', 'Metos3DData/2.8/Geometry/gStartIndices.bin');
-copyfile('MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/gEndIndicesNew.bin', 'Metos3DData/2.8/Geometry/gEndIndices.bin');
+%copyfile('MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/gStartIndicesNew.bin', 'Metos3DData/2.8/Geometry/gStartIndices.bin');
+%copyfile('MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/gEndIndicesNew.bin', 'Metos3DData/2.8/Geometry/gEndIndices.bin');
+
 % tracer initialization
 disp('Tracer initialization ...');
 copyfile('MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/po4ini.petsc', 'Metos3DData/2.8/Initialization/po4ini.petsc');
@@ -161,13 +124,92 @@ rmdir('MIT_Matrix_Global_2.8deg','s')
 
 end
 
+%
+%   make directory structure
+%
+function makeDirectoryStructure
+    disp('Make directory structure ...');
+    mkdir('Metos3DData/2.8')
+    mkdir('Metos3DData/2.8/Forcing')
+    mkdir('Metos3DData/2.8/Forcing/BoundaryCondition')
+    mkdir('Metos3DData/2.8/Forcing/BoundaryCondition/Special')
+    mkdir('Metos3DData/2.8/Forcing/DomainCondition')
+    mkdir('Metos3DData/2.8/Geometry')
+    mkdir('Metos3DData/2.8/Initialization')
+    mkdir('Metos3DData/2.8/Transport')
+    mkdir('Metos3DData/2.8/Transport/Matrix5_4')
+    mkdir('Metos3DData/2.8/Transport/Matrix5_4/1dt')
+end
+
+%
+%   convert data to PETSc format
+%
+function convertDataToPETScFormat
+    disp('Convert data to PETSc format ...');
+    cd 'MIT_Matrix_Global_2.8deg/'
+    cd 'KielBiogeochem_NDOP_Matrix5_4/'
+    % latitude
+    convertBoundaryConditionsToPETSc(1, 'latitude.bin', 4448);
+    % fice
+    convertBoundaryConditionsToPETSc(12, 'fice_', 4448);
+    % depths
+    convertHeightsToDepths
+    cd ..
+    cd ..
+end
+
+%
+%   copy needed files
+%
+function copyNeededFiles
+    disp('Copy needed files ...');
+    copyfile('Matrix/gcm2matrix.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/gcm2matrix.m')
+    copyfile('Misc/read_binary.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/read_binary.m')
+    copyfile('Misc/write_binary.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/write_binary.m')
+    copyfile('MITgcm/rdmds.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/rdmds.m')
+    copyfile('PETSC/writePetscBin.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/writePetscBin.m')
+    copyfile('util/prep/writePetscBinaryMat_mex.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/writePetscBinaryMat_mex.m')
+    copyfile('util/prep/convertBoundaryConditionsToPETSc.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/convertBoundaryConditionsToPETSc.m')
+    copyfile('util/prep/convertHeightsToDepths.m','MIT_Matrix_Global_2.8deg/KielBiogeochem_NDOP_Matrix5_4/convertHeightsToDepths.m')
+end
+
+%
+%   set base path for script from SPK
+%
+function setBasePath
+    disp('Set base path ...');
+    cd 'MIT_Matrix_Global_2.8deg/'
+    base_path = pwd;
+    disp(sprintf('base_path: %s', base_path));
+    save basepath.mat base_path
+    cd 'KielBiogeochem_NDOP_Matrix5_4/'
+    save basepath.mat base_path
+    cd ..
+    cd ..
+end
+
+%
+%   uncompress all needed archives
+%
+function prepareDataFiles
+    disp('Prepare data files ...');
+    uncompressDataFile('Matrix');
+    uncompressDataFile('Misc');
+    uncompressDataFile('MITgcm');
+    uncompressDataFile('PETSC');
+    uncompressDataFile('MIT_Matrix_Global_2.8deg');
+end
+
+%
+%   uncompress a '*.tar.gz' file
+%
 function uncompressDataFile(filename)
     % prepare file name
     filetargz = [filename '.tar.gz'];
     filetar   = [filename '.tar'];
+    disp(sprintf('extract: %s', filetargz));
     % check, if present
     if exist(filetargz, 'file')
-        disp(filetargz);
         gunzip(filetargz);
         untar(filetar);
         delete(filetar);
--- util/prep/convertBoundaryConditionsToPETSc.m
+++ util/prep/convertBoundaryConditionsToPETSc.m
@@ -20,11 +20,11 @@ function convertBoundaryConditionsToPETSc(nfile, filebasename, nprofile);
 
 if (nfile == 1)
     filename = filebasename;
-    disp(filename);
+%    disp(filename);
     % read old file
     fid = fopen(filename, 'r', 'ieee-be');
     v = fread(fid, nprofile, 'real*8');
-    fclose(fid)
+    fclose(fid);
     % write new file
     filename = sprintf('%s.petsc', filebasename);
     disp(filename);
@@ -32,15 +32,15 @@ if (nfile == 1)
     fwrite(fid, 1211214,  'integer*4');                 % VEC_FILE_COOKIE
     fwrite(fid, nprofile, 'integer*4');
     fwrite(fid, v,        'real*8');
-    fclose(fid)
+    fclose(fid);
 else
     for ifile = 1:nfile
         filename = sprintf('%s%02d', filebasename, ifile-1);
-        disp(filename);
+%        disp(filename);
         % read old file
         fid = fopen(filename, 'r', 'ieee-be');
         v = fread(fid, nprofile, 'real*8');
-        fclose(fid)
+        fclose(fid);
         % write new file
         filename = sprintf('%s%02d.petsc', filebasename,ifile-1);
         disp(filename);
@@ -48,6 +48,6 @@ else
         fwrite(fid, 1211214,  'integer*4');             % VEC_FILE_COOKIE
         fwrite(fid, nprofile, 'integer*4');
         fwrite(fid, v,        'real*8');
-        fclose(fid)
+        fclose(fid);
     end
 end
